6c146ceb0eb23f832edd8a5abd8f6363050d7bed
NadishiChristina <nadishiramanayake@gmail.com>
2025-04-22 00:44:26 +0530

    Added Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,60 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./pages/RootLayout";
 import ErrorPage from "./pages/ErrorPage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Results from "./pages/Results";
+import Elections from "./pages/Elections";
+import ElectionDetails from "./pages/ElectionDetails";
+import Candidates from "./pages/Candidates";
+import Congrats from "./pages/Congrats";
+import Logout from "./pages/Logout";
+
+//whenever you have browser path as - http://localhost:5173/(path) - 
+// Output - Navbar (element)
+// http://localhost:5173/congrats - Output - Navbar Congrats
+
+const router = createBrowserRouter([
+  { 
+    path: '/',
+    element: <RootLayout/>,
+    errorElement: <ErrorPage />,
+    children : [
+      {
+        index: true,
+        element: <Login />
+      },
+      {
+        path: "register",
+        element: <Register />
+      },
+      {
+        path: "results",
+        element: <Results />
+      },
+      {
+        path: "elections",
+        element: <Elections />
+      },
+      {
+        path: "elections/:id",
+        element: <ElectionDetails />
+      },
+      {
+        path: "elections/:id/candidates",
+        element: <Candidates />
+      },
+      {
+        path: "congrats",
+        element: <Congrats />
+      },
+      {
+        path: "logout",
+        element: <Logout />
+      }
+    ]
+  }
+])
 
 function App() {
 
