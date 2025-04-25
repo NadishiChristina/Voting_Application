6c57ae589f8fb86bc49e463bfd67fd4ffb9dd76b
NadishiChristina <nadishiramanayake@gmail.com>
2025-04-25 22:35:07 +0530

    imoporting new error page GIF

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import Image from '../assets/404.png'
+import Image from '../assets/404-error-page.gif'
 
 const ErrorPage = () => {
     const Navigate = useNavigate()
