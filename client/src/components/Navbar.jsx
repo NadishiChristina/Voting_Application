0761259bbb2e48baaf9ca5459b3384081e5081ce
NadishiChristina <nadishiramanayake@gmail.com>
2025-04-27 22:42:12 +0530

    Added navbar paths

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="container nav_container">
+      <Link to="/" className='nav_logo'>Vote</Link>
+      <div>
+        <menu>
+          <NavLink to="/elections">Elections</NavLink>
+          <NavLink to="/results">Results</NavLink>
+          <NavLink to="/logout">Logout</NavLink>
+        </menu>
+        <button className="theme_toggle-btn">
+          
+        </button>
+      </div>
+    </div>
   )
 }
 
